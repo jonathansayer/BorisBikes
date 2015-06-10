0b489124ddc8a92bace7b0ed5d1fb8f941bb1dc5
Alex <aleksandernasr@gmail.com>
2015-06-10 12:39:35 +0100

    Created Bike Test - failing because no Bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,14 @@ require 'docking_station'
 
 describe DockingStation do 
 
+  it { is_expected.to respond_to :release_bike }
 
-
-  
+   
 end
+
+describe Bike do 
+  
+
+end
+
+#it { is_expected.to respond_to :working? }
