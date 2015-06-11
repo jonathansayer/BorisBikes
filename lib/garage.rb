d8fcd3fa3ae86327dc46ae20ea48549cb2ecbd51
Gerard Morera <grdmorera@gmail.com>
2015-06-11 23:01:47 +0100

    stage 8

---
+++ lib/garage.rb
@@ -0,0 +1,5 @@
+class Garage
+
+
+
+end
---
+++ spec/bike_container_test_spec.rb
@@ -0,0 +1,10 @@
+require './lib/bike_container'
+require_relative './support/shared_examples_for_bike_container'
+
+class BikeContainerTest
+  include BikeContainer
+end
+
+describe BikeContainerTest do
+  it_behaves_like BikeContainer
+end
