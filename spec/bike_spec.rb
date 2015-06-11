bdc6cd60ffe298b56dd1b8146085ae025a1d4afc
Gerard Morera <grdmorera@gmail.com>
2015-06-11 12:05:03 +0100

    bikes can be realsed and docked

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,8 @@
 require 'bike'
 
+
 describe Bike do 
   
-it { is_expected.to respond_to :working? }
+  it { is_expected.to respond_to :working? }
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,12 @@
 require 'docking_station'
 
+
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
   it 'release working bike' do
+    subject.dock Bike.new
   	bike = subject.release_bike
   	expect(bike).to be_working
   end
@@ -16,6 +18,15 @@ describe DockingStation do
       expect {subject.release_bike}.to raise_error 'No bikes available'
     end    
   end
+
+  describe 'dock' do
+    it 'raise an error when full' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
+    
 end
 
  
